50e656d6a0c9a1c8b344e6376aa2f62fc3b06775
hexadecibal <hex@telios.io>
2021-10-08 10:50:06 -0400

    Enabling deterministic secret box keypairs

--- tests/crypto.test.js
+++ tests/crypto.test.js
@@ -1,9 +1,16 @@
 const tape = require('tape');
 const { Crypto } = require('..');
 
-tape('Crypto', async t => {
+tape('Encrypt message string from AED key', async t => {
   const secret = Crypto.generateAEDKey();
   const encrypted = Crypto.encryptAED('Hey this is a secret message!', secret);
   const deciphered = Crypto.decryptAED(encrypted, secret);
-  t.equals(deciphered, 'Hey this is a secret message!');
+  t.equals(deciphered, 'Hey this is a secret message!', 'Messages match');
+});
+
+tape('Create Secret Box Keypair from String', async t => {
+  const keypair = Crypto.boxKeypairFromStr('This is my test string');
+
+  t.ok(keypair.publicKey, 'Can create public key from string');
+  t.ok(keypair.privateKey, 'Can create private key from string');
 });
